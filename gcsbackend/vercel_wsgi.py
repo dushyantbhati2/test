b1f758a44519c0aa2b14acd0d9c0fecce4e3c8c7
dushyantbhati2 <db90054@gmail.com>
2025-10-29 19:49:28 +0530

    updated

--- gcsbackend/vercel_wsgi.py
+++ gcsbackend/vercel_wsgi.py
@@ -3,7 +3,7 @@
 
 # Import the Django WSGI application and expose it under the names Vercel expects.
 # Your gcsbackend/gcsbackend/wsgi.py already exposes `application`.
-from gcsbackend.wsgi import application as app
+from gcsbackend.gcsbackend.wsgi import application as app
 
 # also provide `handler` as an alias (Vercel accepts either `app` or `handler`)
 handler = app
