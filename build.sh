8cb847c52ea51bb94ff90b9df1e79e05def35218
dushyantbhati2 <db90054@gmail.com>
2025-10-31 15:34:54 +0530

    wsgi fixed

--- build.sh
+++ build.sh
@@ -3,8 +3,9 @@ set -euo pipefail
 
 python -m pip install --upgrade pip
 python -m pip install -r gcsbackend/requirements.txt
+python gcsbackend/manage.py collectstatic --noinput
+
 
 # optional: run migrations if your production DB is reachable during build
 # python gcsbackend/manage.py migrate --noinput
 
-python gcsbackend/manage.py collectstatic --noinput
